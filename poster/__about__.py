c2eebd71bd32b3b34938f429ee07fa3d155c90db
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-02-14 17:24:13 +0900

    adjust pkg install & ci (#10)
    * add checks
    * about
    * setup
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ poster/__about__.py
@@ -0,0 +1,15 @@
+__version__ = "0.1.0"
+__author__ = "Lightning-AI et al."
+__author_email__ = "name@lightning.ai"
+__copyright__ = f"Copyright (c) 2020-2022, {__author__}."
+__homepage__ = "https://github.com/Lightning-AI/markdown-poster"
+__docs__ = "Render Markdowns in your Lightning app"
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__version__",
+]
---
+++ poster/__init__.py
@@ -0,0 +1,4 @@
+from poster.__about__ import *  # noqa: F403
+from poster.component import Poster
+
+__all__ = ["Poster"]
